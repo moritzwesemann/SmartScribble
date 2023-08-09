4286f03a602f3431df2928aac594953419b7d27a
Moritz Wesemann <mwesemann2002@gmail.com>
2023-08-09 11:12:52 +0200

    Initial Commit

---
+++ SmartScribble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartScribble
+//
+//  Created by Moritz on 09.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
