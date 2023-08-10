875585c745e768828f784e582af120d2dee02b3c
Moritz Wesemann <mwesemann2002@gmail.com>
2023-08-10 16:03:58 +0200

    Labels zeigen die jeweiligen Notizen an

--- SmartScribble/ViewController/DetailTagViewController.swift
+++ SmartScribble/ViewController/DetailTagViewController.swift
@@ -7,20 +7,61 @@
 
 import UIKit
 
-class DetailTagViewController: UIViewController {
-
+class DetailTagViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var notesTableView: UITableView!
+    
     var selectedTag: String?
     var tagsArray: [String] = []
+    
+    var notes: [Note] = [] {
+        didSet {
+            filterAndDisplayNotes()
+        }
+    }
+    
+    var filteredNotes: [Note] = [] // Hält die gefilterten Notizen
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(selectedTag)
         self.navigationItem.title = selectedTag
-
-        // Do any additional setup after loading the view.
+        
+        notesTableView.delegate = self
+        notesTableView.dataSource = self
+        
+        // Laden Sie die Notizen aus der Datei
+        if let loadedNotes = Note.loadFromFile() {
+            notes = loadedNotes
+        }
     }
     
+    // Durchsucht die Notizen nach dem ausgewählten Tag und zeigt sie an
+    func filterAndDisplayNotes() {
+        filteredNotes = notes.filter { note in
+            return note.tags.contains(selectedTag ?? "")
+        }
+        
+        // Aktualisiert die TableView, um die gefilterten Notizen anzuzeigen
+        print(filteredNotes)
+        notesTableView.reloadData()
+    }
 
+    // MARK: - Table view data source
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredNotes.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "noteCell", for: indexPath)
+
+        let note = filteredNotes[indexPath.row]
+        cell.textLabel?.text = note.title // Setzt den Titel der jeweiligen Notiz
+
+        return cell
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -30,5 +71,4 @@ class DetailTagViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
