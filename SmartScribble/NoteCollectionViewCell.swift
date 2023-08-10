d0e8be36159a15c5ab467120ded8dc6a5826e29e
Moritz Wesemann <mwesemann2002@gmail.com>
2023-08-10 14:00:38 +0200

    Recent Files hinzugefügt, noch keine sortierunggit add *

---
+++ SmartScribble/NoteCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  NoteCollectionViewCell.swift
+//  SmartScribble
+//
+//  Created by Moritz on 10.08.23.
+//
+
+import UIKit
+
+class NoteCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var textPreviewTextView: UITextView!
+    
+}
--- SmartScribble/NotesCollectionViewController.swift
+++ SmartScribble/NotesCollectionViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
-class NotesCollectionViewController: UIViewController {
-
+class NotesCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    
+    @IBOutlet weak var notesCollectionView: UICollectionView!
+    
+    var notes: [Note] = []
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+           super.viewDidLoad()
+            notesCollectionView.delegate = self
+            notesCollectionView.dataSource = self
+            if let loadedNotes = Note.loadFromFile() {
+                    notes = loadedNotes
+                }
+        print(notes)
+           
+       }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 200) // Stelle die gewünschte Breite und Höhe ein
     }
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+            return 1
+        }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return notes.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = notesCollectionView.dequeueReusableCell(withReuseIdentifier: "noteCell", for: indexPath) as! NoteCollectionViewCell
+            let note = notes[indexPath.row]
+            
+            cell.titleLabel.text = note.title
+            cell.textPreviewTextView.text = String(note.text.prefix(50)) // Zeigt die ersten 50 Zeichen des Textes
+
+            // Rahmen hinzufügen
+            cell.layer.borderWidth = 1.0
+            cell.layer.borderColor = UIColor.black.cgColor // Farbe des Rahmens
+
+            // Abgerundete Ecken hinzufügen
+            cell.layer.cornerRadius = 8.0 // Radius der Ecken
+            
+            return cell
+    }
+    
+
+   
     
 
     /*
